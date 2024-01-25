d2273d42344447ac7dd6aea50705ccc99f7bbd91
Daniil-Tsiunchyk <fcad.td@gmail.com>
2024-01-25 04:44:44 +0300

    init project

---
+++ Telegram-Bot/src/main/java/source/open/telegrambot/TelegramBotApplication.java
@@ -0,0 +1,19 @@
+package source.open.telegrambot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@SpringBootApplication
+public class TelegramBotApplication {
+    public static void main(String[] args) {
+
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
